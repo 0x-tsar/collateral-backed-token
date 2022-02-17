3716f428731d48587a6bd2e38017871d1a2e62dd
Gustavo <contato.gustavofernandes@hotmail.com>
2022-02-17 19:07:58 -0300

    adding identation pattern

---
+++ contracts/CollateralBackedToken.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract CollateralBackedToken is ERC20 {
+  IERC20 public collateral;
+  uint256 public price = 1;
+
+  constructor(address _collateral) ERC20("COLLATERAL BACKED TOKEN", "CBT") {
+    collateral = IERC20(_collateral);
+  }
+
+  function deposit(uint256 collateralAmount) public {
+    //allow first
+    collateral.transferFrom(msg.sender, address(this), collateralAmount);
+    _mint(msg.sender, collateralAmount * price);
+  }
+
+  function withdraw(uint256 tokenAmount) public {
+    require(balanceOf(msg.sender) >= tokenAmount, "Insuficient Balance");
+    _burn(msg.sender, tokenAmount);
+  }
+}
